6df54ef1895347f297f9ceb476c740e87cb00cc9
Gabriel Cerqueira <43377719+gabcerqueira@users.noreply.github.com>
2020-07-11 19:36:04 -0300

    init Links model

---
+++ Links-backend/src/models/link.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+	const Link = sequelize.define("Link", {
+		label: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		url: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		isSocial: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			default: false,
+		},
+	});
+
+	return Link;
+};
