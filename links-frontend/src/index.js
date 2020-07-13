879b4c3d7e0b0e0cfd4a8dead2422b152eaf41e1
Gabriel Cerqueira <43377719+gabcerqueira@users.noreply.github.com>
2020-07-12 21:12:47 -0300

    Added UI kit

--- links-frontend/src/index.js
+++ links-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./main.scss";
+import "./styles/main.scss";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
